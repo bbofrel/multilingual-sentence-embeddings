845c7254370f2701f3602a1ce786dd35a27b5421
bbofrel <tsanda.alyona@gmail.com>
2025-08-16 20:49:15 +0200

    Add raw data loader and update .gitignore

---
+++ data/raw/loading_data.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+
+# Streaming mode
+dataset_stream = load_dataset("opus_books", "en-de", split="train", streaming=True)
+
+# Take first 1000 examples
+subset = []
+for i, example in enumerate(dataset_stream):
+    if i >= 1000:
+        break
+    subset.append(example)
+
+print(len(subset))
+print(subset[0])
