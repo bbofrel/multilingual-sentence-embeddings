061a925bc326871050d9b67e98b97cb3771a73d6
bbofrel <tsanda.alyona@gmail.com>
2025-08-17 20:05:49 +0200

    refined teacher

--- training/train_student.py
+++ training/train_student.py
@@ -6,8 +6,8 @@ def encoding_english_sentences(dataset):
     teacher_model = load_teacher_model()
     parallel_data = dataset_preprocessing()
 
-    english_sentences = [pair[1] for pair in parallel_data]
+    english_sentences = [pair[0] for pair in parallel_data]
     assert isinstance(english_sentences, List)
 
-    embeddings = model.encode(sentences)
+    embeddings = teacher_model.encode(sentences, convert_to_tensor=True)
     return embeddings
